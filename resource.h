68e4ccd2d57f7b3a7ebd5c55e8335e92caa75356
learnrpg <learnrpg@gmail.com>
2014-05-02 05:11:12 +0800

    first update

---
+++ resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by AutoPlay.rc
+//
+#define IDC_MYICON                      2
+#define IDD_AUTOPLAY_DIALOG             102
+#define IDD_ABOUTBOX                    103
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDS_HELLO                       106
+#define IDI_AUTOPLAY                    107
+#define IDI_SMALL                       108
+#define IDC_AUTOPLAY                    109
+#define IDR_MAINFRAME                   128
+#define IDM_SAMPLE                      32770
+#define IDM_MONSTERPRIORITY_LEVEL1      32771
+#define IDM_MONSTERPRIORITY_LEVEL2      32772
+#define IDM_MONSTERPRIORITY_LEVEL3      32773
+#define IDM_DELAYCONTROL                32774
+#define IDM_DELAYCONTROL_1S             32775
+#define IDM_DELAYCONTROL_2S             32776
+#define IDM_DELAYCONTROL_3S             32777
+#define IDM_DELAYCONTROL_4S             32778
+#define IDM_DELAYCONTROL_5S             32779
+#define IDM_DELAYCONTROL_6S             32780
+#define IDM_RUN_START                   32786
+#define IDM_RUN_STOP                    32787
+#define IDM_COMBATACCEL                 32790
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32791
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
